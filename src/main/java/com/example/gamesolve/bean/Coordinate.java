2f41825296623808d80adb2a24a167328dbf939f
Wade <wadechen@outlook.com>
2018-11-28 11:05:02 +0900

    import check

--- src/main/java/com/example/gamesolve/bean/Coordinate.java
+++ src/main/java/com/example/gamesolve/bean/Coordinate.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,7 +13,7 @@ public class Coordinate implements Comparable<Coordinate> {
 
     private int y;
 
-    public final static Coordinate[] fourSides = {new Coordinate(-1,0), new Coordinate(0, 1), new Coordinate(1, 0), new Coordinate(0, -1) };
+    public final static Coordinate[] fourSides = {new Coordinate(-1, 0), new Coordinate(0, 1), new Coordinate(1, 0), new Coordinate(0, -1)};
 
     @Override
     public int compareTo(Coordinate o) {
